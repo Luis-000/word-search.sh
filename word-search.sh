4218f28f3139d6554243f57a2110bbad89b26177
Luis Costa <129795162+Luis-000@users.noreply.github.com>
2026-05-17 13:23:33 +0100

    Add files via upload

---
+++ word-search.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# This Script searches words from names.txt (Just practicing)
+# It then saves the matching words into a found-words.txt file
+
+echo "		
+			--- Instructions ---
+	'^' Before the letter for words that start with the letter;
+	'$' After the letter for words that end with the letter;
+	"
+echo "Enter a letter to search in names.txt (): "
+read letter
+
+# echo "Chosen letter: $letter"
+
+echo "------------"
+
+if [[ $letter == ^* ]]; then
+	echo "Searching for all the words that start with '${letter:1}'..."
+elif [[ $letter == *$ ]]; then
+	echo "Searching for all the words that end with '${letter:0:-1}'..."
+else
+	echo "Searching for all the words with a '$letter' in them..."
+fi
+echo "------------"
+
+i=0
+
+		echo "" >> found-words.txt
+		echo "--------- $(date '+%Y/%m/%d %H:%M:%S') ---------" >> found-words.txt
+		echo "" >> found-words.txt
+
+
+
+while read -r word; do
+	echo "$word" 
+	echo "$word" >> found-words.txt
+
+	((i++))
+done < <(grep -i "$letter" names.txt)
+
+if [[ $i == 0  ]]; then
+	echo "No words were found!"
+else
+	echo "Found $i Words"
+fi
+
+sleep 1
